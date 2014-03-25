064c7ce2046c297171725b97617da9e570177e56
Sam Loesch <sam.loesch@gmail.com>
2014-03-24 19:40:29 -0500

    added a game manager with ability to store paint blobs

---
+++ GPC_shmup/GPC_shmup/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GPC_shmup
+{
+    public class GameManager
+    {
+        private int distanceScrolled;
+
+        private BulletManager _bulletManager;
+        private EnemyManager _enemyManager;
+
+        private List<Vector2> paintBlobStorage = new List<Vector2>();
+
+        public GameManager()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        //TODO: call this method when paint blobs go off screen
+        public void SavePaintBlob(Vector2 relativePosition)
+        {
+            paintBlobStorage.Add(relativePosition + new Vector2(distanceScrolled, 0));
+        }
+    }
+}
